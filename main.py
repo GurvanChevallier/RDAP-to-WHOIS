7f78957e7ffa306c9ebee363e39c8aa84675ede9
GurvanChevallier <gurvan.chevallier@gmail.com>
2024-05-23 17:31:09 +0200

    added the first version of the implementation of all properties (except CLIENTIDMAP, and XML because RFC says it is optional to implement it).

--- main.py
+++ main.py
@@ -182,6 +182,7 @@ def entity_print(role, entity):
             case _:
                 print("ERROR: unrecognized vCard object "+ elem[0])
                 continue
+def vcard_display
 
 def extract_info_from_vcard(vcard): #This extration is reducing the number of fields to only the ones that are expressed, saving potentially a lot of "" elements, in "n" or "adr" fields for an example.
     infos = {}
@@ -311,73 +312,142 @@ def extract_info_from_vcard(vcard): #This extration is reducing the number of fi
                 if elem[2] == "langage-tag":
                     infos["lang"] = elem[3]
                 else:
-                    print("ERROR: " + elem[0] + " is not a language-tag value")
+                    print("ERROR: " + elem[0] + " is not a LANGUAGE-TAG value")
                     return
             case "tz":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
-            case "geo":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "text" or elem[2] == "uri" or elem[2] == "utc-offset":
+                    infos["tz"] = elem[3]
+                    if elem[2] == "utc-offset":
+                        print("WARN: "+ elem[0] + " is a UTC-OFFSET value, not recommended by RFC6350")
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT, URI nor a UTC-OFFSET value")
+                    return
+            case "geo": #RFC 5870
+                if elem[2] == "uri":
+                    infos["geo"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI value")
+                    return
             case "title":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
-            case "role":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "text":
+                    infos["title"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT value")
+                    return
+            case "role": #will be unused in RDAP, because of the roles[] values being used and defined in RFC9083
+                if elem[2] == "text":
+                    infos["role"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT value")
+                    return
             case "logo":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "uri":
+                    infos["logo"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI value")
+                    return
             case "org":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "text":
+                    if type(elem[3]) == str:
+                        infos["org"] = elem[3]
+                    else:
+                        infos["org"]["orgName"] = elem[3][0]
+                        infos["org"]["orgUnits"] = elem[3][1:]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT value")
+                    return
             case "member":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
-            case "related":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if infos.get("kind") == "group":
+                    if elem[2] == "uri":
+                        infos["member"] = elem[3]
+                    else:
+                        print("ERROR: " + elem[0] + " is not a URI value")
+                        return
+                else:
+                    print("ERROR: " + elem[0] + " property MUST NOT be present unless the KIND property is defined as \"group\"")
+                    return
+            case "related": #need to take care to "related-type-value" types in RFC6350 section 6.6.6
+                if elem[2] == "uri" or elem[2] == "text":
+                    infos["related"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI nor a TEXT value")
             case "categories":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "text":
+                    infos["categories"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT value")
+                    return
             case "note":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "text":
+                    infos["note"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a text value")
+                    return
             case "prodid":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "text":
+                    infos["prodid"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT value")
+                    return
             case "rev":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "timestamp":
+                    infos["rev"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TIMESTAMP value")
+                    return
             case "sound":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "uri":
+                    infos["sound"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI value")
+                    return
             case "uid":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
-            case "clientpidmap":
+                if elem[2] == "uri" or elem[2] == "text":
+                    infos["uid"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI nor a TEXT value")
+            case "clientpidmap": # seems painful to implement sect6.7.7
                 print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
                 continue
             case "url":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "uri":
+                    infos["url"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI value")
+                    return
             case "version":
-                infos["version"] = elem[3]
+                if elem[2] == "text":
+                    infos["version"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT value")
+                    return
             case "key":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
-            case "fburl":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
-            case "caladruri":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
-            case "caluri":
-                print("\"" + elem[0] + "\" is not implemented yet, but is detected.")
-                continue
+                if elem[2] == "text" or elem[2] == "uri":
+                    infos["key"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a TEXT nor a URI value")
+                    return
+            case "fburl": #need the pref character if multiple instances are specified
+                if elem[2] == "uri":
+                    infos["fburl"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI value")
+            case "caladruri": #need the pref character if multiple instances are specified
+                if elem[2] == "uri":
+                    infos["caladruri"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI value")
+            case "caluri": #need the pref character if multiple instances are specified
+                if elem[2] == "uri":
+                    infos["caluri"] = elem[3]
+                else:
+                    print("ERROR: " + elem[0] + " is not a URI value")
             case _:
                 print("ERROR: unrecognized vCard object " + elem[0])
                 continue
+        if infos.get("version") is None or infos.get("version") != "4.0": # RFC6350 sect. 6.7.9 (maybe not in jCard we'll see)
+            print("ERROR: Version of the jCard missing/misplaced")
+            return
     if not "kind" in infos:
         infos["kind"] = "individual"
 
