9d09fada61d646010e9eb52e893acd37fc677954
GurvanChevallier <gurvan.chevallier@gmail.com>
2024-06-03 16:36:59 +0200

    whois-rdap-translation.py: added the support of the Registrar infos, except IANA ID.
    Added looping for a number of lines, translation being done on every line of it.
    
    Added review over the unrecognized fields in vCardArrays at the end of the sample translations

--- whois-rdap-translation.py
+++ whois-rdap-translation.py
@@ -4,25 +4,41 @@ import vcard
 import csv
 import tldextract
 import json
-nbsamples = 2
+nbsamples = 10000
 
 def display_whois_data(whois_data):
-    unrecognized = []
+    unrecognized = {}
     if whois_data.get("domain_name"):
         print("Domain Name: " + whois_data.get("domain_name"))
     if whois_data.get("registry_domain_id"):
         print("Registry Domain ID: " + whois_data.get("registry_domain_id"))
     if whois_data.get("registrar_rdap_server"):
-        print("Registrat RDAP Server: " + whois_data.get("registrar_rdap_server"))
+        print("Registrant RDAP Server: " + whois_data.get("registrar_rdap_server"))
     if whois_data.get("lastChangedDate"):
         print("Updated Date : " + whois_data.get("lastChangedDate"))
     if whois_data.get("creationDate"):
         print("Creation Date : " + whois_data.get("creationDate"))
     if whois_data.get("expirationDate"):
         print("Registrar Registration Exipration Date : " + whois_data.get("expirationDate"))
-    # TODO: REGISTRAR INFOS TO PRINT, ESPECIALLY THE ABUSE THINGY
     #pprint()
-    #print("REGISTRAR EXISTS, NEED TO BE IMPLEMENTED SOON")
+    if whois_data.get("entities"):
+        for entity in whois_data.get("entities"):
+            for role in entity:
+                for vcard_elem in entity.get(role):
+                    if role == "registrar":
+                        match vcard_elem:
+                            case "fn":
+                                print(role.capitalize() + ": " + entity.get(role).get("fn"))
+                            case _:
+                                continue
+                    if role == "abuse":
+                        match vcard_elem:
+                            case "email":
+                                print("Registrar Abuse Conctact Email: " + entity.get(role).get("email"))
+                            case "tel":
+                                print("Registrar Abuse Contact Phone: " + entity.get(role).get("tel")[4:])
+                            case _:
+                                continue
     if whois_data.get("eppcodes"):
         for eppcode in whois_data.get("eppcodes"):
             print("Domain Status: "+ eppcode)
@@ -30,13 +46,13 @@ def display_whois_data(whois_data):
      #different registrant/admin/tech infos
     if whois_data.get("entities"):
         for entity in whois_data.get("entities"):
-            if entity.get("registrar"):
+            if entity.get("registrar") or entity.get("abuse"):
                 continue
             for role in entity:
                 for vcard_elem in entity.get(role):
                     match vcard_elem:
                         case "fn":
-                            print(role.capitalize() + " Name: " + entity.get(role).get("org"))
+                            print(role.capitalize() + " Name: " + entity.get(role).get("fn"))
                             continue
                         case "org":
                             print (role.capitalize() + " Organization: " + entity.get(role).get("org"))
@@ -78,8 +94,13 @@ def display_whois_data(whois_data):
                             print(role.capitalize() + " Email: " + entity.get(role).get("email"))
                             continue
                         case "unrecognized":
-                            if unrecognized:
-                                entity.get(role).get("email")
+                            if entity.get(role).get("unrecognized"):
+                                for elem_unrecognized in entity.get(role).get("unrecognized"):
+                                    if elem_unrecognized not in unrecognized:
+                                        unrecognized[elem_unrecognized] = 1
+                                    else:
+                                        unrecognized[elem_unrecognized] += 1
+                                pass #maybe try to show something after the whois translation displayed
                         case _:
                             continue
 
@@ -89,84 +110,110 @@ def display_whois_data(whois_data):
     if whois_data.get("secure_dns"):
         print("Secure DNS: " + whois_data.get("secure_dns"))
 
-    return
+    print("\n\n")
+    return unrecognized
 
-def get_rdap_data():
-    with open("RDAP_sample.json", "r") as samplefile:
-        for i in range(0, int(nbsamples)):
-            sample_json = json.loads(samplefile.readline())
-            ## rejecting the cases where analysis of the RDAP response will be useless/impossible ##
-            if sample_json.get("mode") != "rdap":
-                print("INFO: Sample with a mode not fitting (!=rdap), skipping")
+def get_rdap_data(sample_json):
+    ## rejecting the cases where analysis of the RDAP response will be useless/impossible ##
+    if sample_json.get("mode") != "rdap":
+        print("INFO: Sample with a mode not fitting (!=rdap), skipping")
+        return
+    if sample_json.get("status_code")[0] != "200":
+        print("INFO: Sample with a status code not fitting (!=200), skipping")
+        return
+    if sample_json.get("val") == "Cannot read the body":
+        print("INFO: Sample with RDAP response body not readable, skipping")
+        return
+
+    lookup_sample = json.loads(sample_json.get("val"))
+    print("FROM RDAP:\n\n")
+    pprint(lookup_sample)
+    print("\n\n TO WHOIS: \n\n")
+    whois_data = {}
+    for elem in lookup_sample:
+        match elem:
+            case "handle":
+                whois_data["registry_domain_id"] = lookup_sample.get(elem)
+            case "ldhName":
+                whois_data["domain_name"] = lookup_sample.get(elem)
+            case "links":
+                for link in lookup_sample.get("links"):
+                    if link.get("rel") != "self" and link.get("type") == "application/rdap+json":
+                        whois_data["registrar_rdap_server"] = tldextract.extract(link.get("href")).fqdn
+            case "nameservers":
+                whois_data["nameservers"] = []
+                for ns in lookup_sample.get("nameservers"):
+                    whois_data["nameservers"].append(ns.get("ldhName"))
+            case "secureDNS":
+                whois_data["secure_dns"] = "signedDelegation" if lookup_sample.get(elem).get(
+                    "delegationSigned") == True else "unsigned"
+            case "events":
+                for event in lookup_sample.get("events"):
+                    match event.get("eventAction"):
+                        case "registration":
+                            whois_data["registrationDate"] = event.get("eventDate")
+                        case "expiration":
+                            whois_data["expirationDate"] = event.get("eventDate")
+                        case "last changed":
+                            whois_data["lastChangedDate"] = event.get("eventDate")
+                        case _:
+                            continue
+            case "entities":
+                whois_data["entities"] = []
+                for entity in lookup_sample.get("entities"):
+                    roles = entity.get("roles")
+                    for role in roles:
+                        #pprint(entity)
+                        if entity.get("vcardArray"):
+                            whois_data["entities"].append({role: vcard.extract_info_from_vcard(entity.get("vcardArray")[1])})
+                        if role == "registrar":
+                            ## TODO: STILL HAVE TO RETRIEVE THE IANA ID AND ADD IT INTO THE WHOIS_DATA dict INSIDE OF THE "registrar" object in entities[]
+                            if entity.get("entities"):
+                                for registrar_entity in entity.get("entities"):
+                                    if "abuse" in registrar_entity.get("roles"):
+                                        whois_data["entities"].append({"abuse": vcard.extract_info_from_vcard(
+                                            registrar_entity.get("vcardArray")[1])})
+
                 continue
-            if sample_json.get("status_code")[0] != "200":
-                print("INFO: Sample with a status code not fitting (!=200), skipping")
+
+            case "status":
+                with open("rdap-json-values-status.csv", 'r') as file:
+                    csvfile = csv.DictReader(file, delimiter=",")
+                    listed_epp = list(csvfile)
+                epp_code_statuses = []
+
+                # get only the list of EPPs that has a defined EPPCode to return in whois format
+                for epp in listed_epp:
+                    if epp['EPPCode'] is not None and epp['Type'] == "status":
+                        epp_code_statuses.append(epp)
+                whois_data["eppcodes"] = []
+                # get each status line, compare it with the list of EPP status codes created just before
+                for status in lookup_sample.get(elem):
+                    for eppcode in listed_epp:
+                        if eppcode['Value'] == status:
+                            whois_data["eppcodes"].append(eppcode.get("EPPCode"))
+                            break
                 continue
-            if sample_json.get("val") == "Cannot read the body":
-                print("INFO: Sample with RDAP response body not readable, skipping")
+            case _:
                 continue
-
-            lookup_sample = json.loads(sample_json.get("val"))
-            pprint(lookup_sample.get("entities"))
-            whois_data = {}
-            for elem in lookup_sample:
-                match elem:
-                    case "handle":
-                        whois_data["registry_domain_id"] = lookup_sample.get(elem)
-                    case "ldhName":
-                        whois_data["domain_name"] = lookup_sample.get(elem)
-                    case "links":
-                        for link in lookup_sample.get("links"):
-                            if link.get("rel") != "self" and link.get("type") == "application/rdap+json":
-                                whois_data["registrar_rdap_server"] = tldextract.extract(link.get("href")).fqdn
-                    case "nameservers":
-                        whois_data["nameservers"] = []
-                        for ns in lookup_sample.get("nameservers"):
-                            whois_data["nameservers"].append(ns.get("ldhName"))
-                    case "secureDNS":
-                        whois_data["secure_dns"] = "signedDelegation" if lookup_sample.get(elem).get("delegationSigned") == True else "unsigned"
-                    case "events":
-                        for event in lookup_sample.get("events"):
-                            match event.get("eventAction"):
-                                case "registration":
-                                    whois_data["registrationDate"] = event.get("eventDate")
-                                case "expiration":
-                                    whois_data["expirationDate"] = event.get("eventDate")
-                                case "last changed":
-                                    whois_data["lastChangedDate"] = event.get("eventDate")
-                                case _:
-                                    continue
-                    case "entities":
-                        whois_data["entities"] = []
-                        for entity in lookup_sample.get("entities"):
-                            roles = entity.get("roles")
-                            for role in roles:
-                                whois_data["entities"].append({role: vcard.extract_info_from_vcard(entity.get("vcardArray")[1])})
-                                if role == "registrar":
-                                    pass # DES CHOSES A FAIRE POUR RECUPERER LE ABUSE ET LE METTRE DANS LE REGISTRAR
-                        continue
-
-                    case "status":
-                        with open("rdap-json-values-status.csv", 'r') as file:
-                            csvfile = csv.DictReader(file, delimiter=",")
-                            listed_epp = list(csvfile)
-                        epp_code_statuses = []
-
-                        # get only the list of EPPs that has a defined EPPCode to return in whois format
-                        for epp in listed_epp:
-                            if epp['EPPCode'] is not None and epp['Type'] == "status":
-                                epp_code_statuses.append(epp)
-                        whois_data["eppcodes"] = []
-                        # get each status line, compare it with the list of EPP status codes created just before
-                        for status in lookup_sample.get(elem):
-                            for eppcode in listed_epp:
-                                if eppcode['Value'] == status:
-                                    whois_data["eppcodes"].append(eppcode.get("EPPCode"))
-                                    break
-                        continue
-                    case _:
-                        continue
+    #print("\n\nWHOISDATA:\n")
+    #pprint(whois_data)
+    #print("\n")
     return whois_data
 
-retrieved_data = get_rdap_data()
-display_whois_data(retrieved_data)
+with open("RDAP_sample.json", "r") as samplefile:
+    unrecognized_vcards = {}
+    for i in range(0, nbsamples):
+        content = samplefile.readline()
+        retrieved_data = get_rdap_data(json.loads(content))
+        if retrieved_data:
+            unrec = display_whois_data(retrieved_data)
+            for elem_unrecognized_rn in unrec:
+                    if elem_unrecognized_rn not in unrecognized_vcards:
+                        unrecognized_vcards[elem_unrecognized_rn] = 1
+                    else:
+                        unrecognized_vcards[elem_unrecognized_rn] += 1
+
+    print("For "+ str(nbsamples) + " samples tested, unrecognized properties in vCards:")
+    print(unrecognized_vcards)
+
